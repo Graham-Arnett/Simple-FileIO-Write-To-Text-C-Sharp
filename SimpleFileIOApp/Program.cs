d7054f03d3497934f1c09ee3c709dbb32bb8e178
Graham Arnett <GrahamKentArnett@outlook.com>
2024-12-14 23:49:33 -0500

    init commit

---
+++ SimpleFileIOApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+
+namespace SimpleFileIOApp
+{
+    internal class Program
+    {
+
+        static void WriteFile(string Filename) 
+        {
+            File.WriteAllText(Filename, "Hello World!");
+        }
+
+        static void ReadFile(string Filename) 
+        {
+            //File.ReadAllText(Filename);
+            Console.WriteLine(File.ReadAllText(Filename));
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Welcome to my simple fileIO practice app");
+
+            string Filename = @"C:mydoc.txt";
+
+            WriteFile(Filename);
+
+            ReadFile(Filename);
+
+            Console.Read();
+        }
+    }
+}
